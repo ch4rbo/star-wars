6787d60faf9d0a2dd74e737526403f8e46c33bf5
ch4rbo <baueratctu@gmail.com>
2021-10-05 00:49:51 +0000

    agrego botón favoritos

--- src/js/component/tarjeta.js
+++ src/js/component/tarjeta.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
 
 const Tarjeta = props => {
+	const { store, actions } = useContext(Context);
 	return (
-		<div className="card" style={{ width: "18rem" }}>
+		<div className="card" style={{ width: "auto", display: "inline-block", margin: "5px" }}>
 			<img className="card-img-top" src="..." alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.name}</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the cards content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
+				<p className="card-text">Learn More Learn More ASDKÑLASLDAS[D</p>
+				<div className="d-flex justify-content-between">
+					<a href="#" className="btn btn-primary">
+						Star Wars Fandome
+					</a>{" "}
+					<button
+						type="button"
+						className="btn btn-outline-warning"
+						onClick={actions.setFavorites(props.name)}>
+						<i className="far fa-heart" />
+					</button>
+				</div>{" "}
 			</div>
 		</div>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,15 +7,17 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 	return (
 		<div className="container-fluid">
-			<div className="row">
-				<div className="scroll">
-					{store.listaPersonajes.map((personaje, index) => {
-						return <Tarjeta key={index} name={personaje.name} />;
-					})}
-				</div>
+			<div
+				className="scrolling-wrapper row flex-row flex-nowrap row-cols-1 row-cols-md-3"
+				style={{ overflowX: "scroll", overflowY: "hidden", whiteSpace: "nowrap" }}>
+				{store.listaPersonajes.map((personaje, index) => {
+					return <Tarjeta key={index} name={personaje.name} />;
+				})}
 			</div>
 
-			<div className="row">
+			<div
+				className="scrolling-wrapper row flex-row flex-nowrap row-cols-1 row-cols-md-3"
+				style={{ overflowX: "scroll", overflowY: "hidden", whiteSpace: "nowrap" }}>
 				{store.listaPlanetas.map((planeta, index) => {
 					return <Tarjeta key={index} name={planeta.name} />;
 				})}
